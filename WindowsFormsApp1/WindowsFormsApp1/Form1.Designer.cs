b815f528d3d9b0d538feac4d4a48042dc7bddd05
İbrahim DOĞAN <33070834+ibrahimdoqn@users.noreply.github.com>
2019-05-12 03:15:55 +0300

    Release

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(42, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CPU Temp";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(46, 102);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Manuel Kontrol";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(46, 73);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(132, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Otomatik Kontrol";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Enabled = false;
+            this.hScrollBar1.Location = new System.Drawing.Point(10, 166);
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(201, 17);
+            this.hScrollBar1.TabIndex = 3;
+            this.hScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.HScrollBar1_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(12, 210);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(199, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Wantto Fan Kontrol";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.NotifyIcon1_MouseDoubleClick);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(46, 186);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(132, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(42, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "GPU Temp";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(54, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Info Screen";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(219, 239);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Wantto PWM";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+using OpenHardwareMonitor.Hardware;
+using Microsoft.Win32;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            hScrollBar1.Minimum = 40;
+            hScrollBar1.Maximum = 100;
+            notifyIcon1.Visible = true;
+            string[] portlar = SerialPort.GetPortNames(); // Bağlı seri portları diziye aktardık
+            foreach (string portAdi in portlar)
+            {
+                comboBox1.Items.Add(portAdi);
+            }
+            try {comboBox1.SelectedIndex = 0;}
+            catch { MessageBox.Show("Arduino bulunamadı"); }
+        }
+
+
+        private void seriPort(int tempcpu){
+            SerialPort seriPort;
+            seriPort = new SerialPort();
+            seriPort.PortName = comboBox1.Text;
+            seriPort.BaudRate = 9600;
+            seriPort.Open();
+            if (tempcpu > 79) seriPort.Write("w");
+            else if (tempcpu > 78) seriPort.Write("e");
+            else if (tempcpu > 77) seriPort.Write("r");
+            else if (tempcpu > 76) seriPort.Write("t");
+            else if (tempcpu > 75) seriPort.Write("y");
+            else if (tempcpu > 74) seriPort.Write("u");
+            else if (tempcpu > 73) seriPort.Write("o");
+            else if (tempcpu > 72) seriPort.Write("p");
+            else if (tempcpu > 71) seriPort.Write("a");
+            else if (tempcpu > 70) seriPort.Write("s");
+            else if (tempcpu > 69) seriPort.Write("d");
+            else if (tempcpu > 68) seriPort.Write("f");
+            else if (tempcpu > 67) seriPort.Write("g");
+            else if (tempcpu > 66) seriPort.Write("h");
+            else if (tempcpu > 65) seriPort.Write("j");
+            else if (tempcpu > 64) seriPort.Write("k");
+            else if (tempcpu > 63) seriPort.Write("l");
+            else if (tempcpu > 62) seriPort.Write("i");
+            else if (tempcpu > 61) seriPort.Write("z");
+            else if (tempcpu > 60) seriPort.Write("x");
+            else if (tempcpu > 59) seriPort.Write("c");
+            else if (tempcpu > 58) seriPort.Write("v");
+            else if (tempcpu > 57) seriPort.Write("b");
+            else if (tempcpu > 56) seriPort.Write("n");
+            else if (tempcpu > 55) seriPort.Write("m");
+            else if (tempcpu > 54) seriPort.Write("0");
+            else if (tempcpu > 53) seriPort.Write("1");
+            else if (tempcpu > 52) seriPort.Write("2");
+            else if (tempcpu > 51) seriPort.Write("3");
+            else if (tempcpu > 50) seriPort.Write("4");
+            else if (tempcpu > 48) seriPort.Write("5");
+            else if (tempcpu > 46) seriPort.Write("6");
+            else if (tempcpu > 44) seriPort.Write("7");
+            else if (tempcpu > 42) seriPort.Write("8");
+            else seriPort.Write("9");
+            seriPort.Close();
+}
+        private int maxSicaklik = 0;
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            Computer computer = new Computer() { CPUEnabled = true, GPUEnabled = false };
+            Computer gpu = new Computer() { CPUEnabled = false, GPUEnabled = true };
+            computer.Open();
+            int tempcpu = 0;
+            int tempgpu = 0;
+            foreach (IHardware hardware in computer.Hardware)
+            {
+                hardware.Update();
+                
+                foreach (ISensor sensor in hardware.Sensors)
+                {
+                    // Celsius is default unit
+
+                    if (sensor.SensorType == SensorType.Temperature)
+                    {
+                        tempcpu = (tempcpu + Convert.ToInt32(sensor.Value)) / 2;
+                    }
+                }
+                label1.Text ="Cpu Sıcaklığı = " + Convert.ToString(tempcpu);
+
+            }
+
+            gpu.Open();
+            foreach (IHardware hardware in gpu.Hardware)
+            {
+                hardware.Update();
+
+                foreach (ISensor sensor in hardware.Sensors)
+                {
+                    // Celsius is default unit
+                    if (sensor.SensorType == SensorType.Temperature)
+                    {
+                        tempgpu = Convert.ToInt32(sensor.Value);
+                    }
+                }
+                label3.Text = "Gpu Sıcaklığı = " + Convert.ToString(tempgpu);
+            }
+            label4.Text = "  Ortalama sıcaklık " + ((tempcpu + tempgpu) / 2);
+
+            if (tempcpu > tempgpu)
+            {
+                if (tempcpu > maxSicaklik) maxSicaklik = tempcpu;
+            }
+            else
+            {
+                if (tempgpu > maxSicaklik) maxSicaklik = tempgpu;
+            }
+            label4.Text = label4.Text + "\nMaxiumum Sıcaklık " + Convert.ToString(maxSicaklik);
+            if (tempcpu > 75 || tempgpu > 75)
+                {
+                    NotifyBildirim("Yüksek sıcaklık değeri", "Lütfen sıcaklığı kontrol edin");
+                }
+            if (button2.Enabled == false)
+            {
+                if (tempcpu > tempgpu) seriPort(tempcpu);
+                else seriPort(tempgpu);
+            }
+            if (FormWindowState.Minimized == this.WindowState) this.Hide();
+            notifyIcon1.Text = "CPU Temp: " + Convert.ToString(tempcpu) + " GPU Temp : " + Convert.ToString(tempgpu);
+        }
+
+        private void NotifyBildirim(string baslik, string mesaj)
+        {
+            notifyIcon1.BalloonTipText = mesaj;
+            notifyIcon1.BalloonTipIcon = ToolTipIcon.Info;
+            notifyIcon1.BalloonTipTitle = baslik;
+            notifyIcon1.ShowBalloonTip(3000);
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            SerialPort seriPort;
+            seriPort = new SerialPort();
+            seriPort.PortName = comboBox1.Text;
+            seriPort.BaudRate = 9600;
+            seriPort.Open();
+            seriPort.Write("q");
+            seriPort.Close();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = true;
+            hScrollBar1.Enabled = true;
+        }
+
+        private void HScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            seriPort(hScrollBar1.Value);
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            button1.Enabled = true;
+            button2.Enabled = false;
+            hScrollBar1.Enabled = false;
+        }
+
+        private void NotifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
